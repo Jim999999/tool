d9efd8cffeb26565092cc4d0a2e949c27d095a8b
jim <jim@163.com>
2020-07-27 13:57:22 +0800

    ed

--- nodejs-test/koa2.js
+++ nodejs-test/koa2.js
@@ -1,7 +1,11 @@
-const Koa = require('koa2')
+const Koa = require('koa2') //引入Koa2类
 const router = require('koa-router')() //require('koa-router')返回的是函数
 const bodyParser = require('koa-bodyparser')
 const fs = require('fs')
+const views = require('koa-views')
+const session = require('koa-session')
+
+
 const app = new Koa()
 const port = 3000
 
@@ -14,32 +18,77 @@ const port = 3000
 
 
 //use bodyParser 应在use router之前
-app.use(bodyParser())
+app.use(bodyParser()) //koa-bodyparser必须在router之前被注册到app对象上
+
+
+// 模板的后缀名是.html
+// app.use(views('views', {
+//     map: {html: 'ejs'} 
+// }));
+
+//配置模板引擎
+app.use(views('views', {
+    extension: 'ejs'
+}))
+
+
+//配置session
+app.keys = ['some secret hurr']; /**cookie的签名 默认*/
+const CONFIG = {
+    /** 默认 */
+    key: 'koa:sess',
+    /**  cookie的过期时间 */
+    maxAge: 10000,
+    /** 默认 可以重写过期时间 */
+    overwrite: true,
+    /**  true表示只有服务器端可以获取 cookie */
+    httpOnly: true,
+    /** 默认 签名 */
+    signed: true,
+    /** 在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false） */
+    rolling: true,
+    /** 当用户进行浏览器操作时刷新 cookie 过期时间 */
+    renew: false,
+
+};
+app.use(session(CONFIG, app));
+
+
 
 //引入router
 app.use(router.routes())
 
 
 app.use(async(ctx, next) => {
-
-    await next() //用use 调用中间件时，调用下一个函数
     ctx.response.type = 'text/html';
-    // ctx.response.body = 'Hello, koa2! ' + Math.round(Math.random(100) * 100, 2);
+    //ctx.response.body = 'Hello, koa2! ' + Math.round(Math.random(100) * 100, 2);
 
+    console.log('我是一个中间件')
+
+    await next() //用use 调用中间件时，调用下一个 middleware 函数
 
 })
 
-router.get('/', function(ctx, next) {
+
+router.get('/', async(ctx, next) => {
+
+    //设置session
+    ctx.session.userinfo = '张三';
+    //获取session
+    //console.log(ctx.session.userinfo);
 
     //当前目录下所有的js文件
     let files = fs.readdirSync('./')
     let jsfiles = files.filter((f) => {
         return f.endsWith('.js')
     })
-    console.log(jsfiles)
 
+    //console.log(jsfiles)
 
-    ctx.response.body = 'index'
+    await ctx.render('index', {
+        title: 'hello word index'
+    });
+    //ctx.response.body = 'index'
 })
 
 
@@ -65,7 +114,7 @@ router.get('/addUser', async(ctx, next) => {
 
 router.post('/saveUser', async(ctx, next) => {
 
-    let name = ctx.request.body.u_name || ''
+    let name = ctx.request.body.u_name || '' //提示 u_name undefined 时，注意 koa-bodyparser有无use引用，并且是在router之前被引用
     let password = ctx.request.body.password || ''
     console.log('addUser ：' + name)
 
@@ -83,6 +132,7 @@ router.get('/:id', async(ctx, next) => {
 
     ctx.response.body = "Get the param : " + id
 })
+
 
 
 
